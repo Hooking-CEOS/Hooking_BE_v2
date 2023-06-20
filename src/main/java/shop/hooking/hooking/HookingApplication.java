77ac902e4f05ea5e13a57d2e9b34394d0fe2db4f
Jiwon <you@example.com>
2023-06-21 00:45:38 +0900

    init: 초기환경 세팅

---
+++ src/main/java/shop/hooking/hooking/HookingApplication.java
@@ -0,0 +1,14 @@
+package shop.hooking.hooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class HookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/shop/hooking/hooking/HookingApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.hooking.hooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
