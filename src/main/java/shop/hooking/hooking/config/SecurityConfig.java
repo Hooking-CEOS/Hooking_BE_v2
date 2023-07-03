fe7072ae5754711c3f34b3bfe20ae7969c3b6108
CYJhub <yejee1018@naver.com>
2023-07-03 20:12:33 +0900

    [feat] /brand 권한 해제

--- src/main/java/shop/hooking/hooking/config/SecurityConfig.java
+++ src/main/java/shop/hooking/hooking/config/SecurityConfig.java
@@ -55,7 +55,7 @@ public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
             .and()
             .authorizeRequests()
             .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
-            .antMatchers("**/oauth2/**","/kakaologin","/api/v1/users/signup/**", "/api/v1/users/login",
+            .antMatchers("**/oauth2/**","/kakaologin","/brand", "/api/v1/users/signup/**", "/api/v1/users/login",
 
                     "/api/v1/users/re-issue", "/api/v1/settings", "/api/v1/users/certification/**",
                     "/api/v1/notification/subscribe", "/api/v1/util/**").permitAll()
