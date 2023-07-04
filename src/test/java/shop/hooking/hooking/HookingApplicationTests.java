a183940b7243154240c9006ce17d031dc0fd6172
CYJhub <yejee1018@naver.com>
2023-07-04 21:19:21 +0900

    [feat] querydsl 기본 구조 생성

--- src/test/java/shop/hooking/hooking/HookingApplicationTests.java
+++ src/test/java/shop/hooking/hooking/HookingApplicationTests.java
@@ -2,12 +2,20 @@ package shop.hooking.hooking;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import shop.hooking.hooking.entity.QCard;
+
+import java.util.List;
 
 @SpringBootTest
 class HookingApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public List<card> dynamicdsl() throws Exception {
+		String username
 	}
 
 }
