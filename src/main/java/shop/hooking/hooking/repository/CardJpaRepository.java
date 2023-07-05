1837728fd6ca6efa464297f7638e7618d0b5de1b
CYJhub <yejee1018@naver.com>
2023-07-05 16:46:53 +0900

    [feat] mood 필터 적용

--- src/main/java/shop/hooking/hooking/repository/CardJpaRepository.java
+++ src/main/java/shop/hooking/hooking/repository/CardJpaRepository.java
@@ -1,20 +1,19 @@
 package shop.hooking.hooking.repository;
 
-import com.querydsl.core.types.Predicate;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.stereotype.Repository;
 import shop.hooking.hooking.dto.CardSearchCondition;
 import shop.hooking.hooking.dto.response.CopyRes;
 import shop.hooking.hooking.dto.response.QCopyRes;
+import shop.hooking.hooking.entity.*;
 
 import javax.persistence.EntityManager;
 import java.util.List;
-
-import static org.springframework.util.StringUtils.hasText;
 import static shop.hooking.hooking.entity.QBrand.brand;
 import static shop.hooking.hooking.entity.QCard.card;
-
+import static shop.hooking.hooking.entity.QHave.have;
+import static shop.hooking.hooking.entity.QMood.mood;
 @Repository
 public class CardJpaRepository {
 
@@ -29,6 +28,12 @@ public class CardJpaRepository {
     }
 
     public List<CopyRes> search(CardSearchCondition condition){
+
+        String moodString = condition.getMood();
+        String[] moods = null; // null처리 해줘야함
+        if (moodString != null) {
+            moods = moodString.split(",");
+        }
 
         String priceString = condition.getPrice();
         String[] prices = null; // null처리 해줘야함
@@ -46,8 +51,14 @@ public class CardJpaRepository {
         String productString = condition.getProduct();
         String[] products = null;
         if (productString != null) {
-            products = productString.split(",");
+            products = productString.split(","); // 리스트 형태로 삽입
         }
+
+        QHave have = QHave.have;
+        QBrand brand = QBrand.brand;
+        QMood mood = QMood.mood;
+
+
 
         return queryFactory
                 .select(new QCopyRes(
@@ -57,13 +68,30 @@ public class CardJpaRepository {
                         card.scrapCnt,
                         card.createdAt))
                 .from(card)
-                .leftJoin(card.brand, brand) // 조인
+                .leftJoin(card.brand, brand)
+                .leftJoin(have)
+                .on(have.brand.eq(brand))
+                .join(have.mood, mood)
                 .where(
                         productEq(products),
                         ageEq(ages),
-                        priceEq(prices)
+                        priceEq(prices),
+                        moodEq(moods)
                 )
                 .fetch();
+    }
+
+
+
+    private BooleanExpression moodEq(String[] moods) { // 스킨케어, 색조화장
+        BooleanExpression moodExpression = null;
+        if (moods != null ) {
+            for (String mood : moods) {
+                BooleanExpression condition = QMood.mood.moodName.eq(mood);
+                moodExpression = (moodExpression != null) ? moodExpression.or(condition) : condition;
+            }
+        }
+        return moodExpression;
     }
 
     private BooleanExpression productEq(String[] products) { // 스킨케어, 색조화장
@@ -89,26 +117,17 @@ public class CardJpaRepository {
         return ageExpression;
     }
 
-    private BooleanExpression priceEq(String[] prices) {
+    private BooleanExpression priceEq(String[] prices) { //[저가라인, 중저가라인, 고가라인]
         BooleanExpression priceExpression = null;
         if (prices != null ) {
             for (String price : prices) {
                 BooleanExpression condition = brand.brandPrice.eq(price);
                 priceExpression = (priceExpression != null) ? priceExpression.or(condition) : condition;
+                // (brand.brandPrice.eq("저가라인")).or(brand.brandPrice.eq("중저가라인")).or(brand.brandPrice.eq("고가라인"))
+                // brand.brandPrice = "저가라인" OR brand.brandPrice = "중저가라인" OR brand.brandPrice = "고가라인"
             }
         }
         return priceExpression;
     }
-//private BooleanExpression priceEq(String price) {
-//    return hasText(price) ? brand.brandPrice.eq(price) : null;
-//}
-//
-//    private BooleanExpression ageEq(String age) {
-//        return hasText(age) ? brand.brandAge.eq(age) : null;
-//    }
-//
-//    private BooleanExpression productEq(String product) {
-//        return hasText(product) ? brand.brandProduct.eq(product) : null;
-//    }
 
 }
