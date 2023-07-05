890ceb6b25bbc0e6f0f7aa5a071c64813b3d24f6
Jiwon <you@example.com>
2023-07-05 13:51:11 +0900

    [feat] 다중 필터기능 생각

--- src/main/java/shop/hooking/hooking/repository/CardJpaRepository.java
+++ src/main/java/shop/hooking/hooking/repository/CardJpaRepository.java
@@ -39,44 +39,58 @@ public class CardJpaRepository {
                 .from(card)
                 .leftJoin(card.brand, brand) // 조인
                 .where(
-                        productEq(condition.getProducts()),
-                        ageEq(condition.getAges()),
-                        priceEq(condition.getPrices())
+                        productEq(condition.getProduct()),
+                        ageEq(condition.getAge()),
+                        priceEq(condition.getPrice())
                 )
                 .fetch();
     }
 
-    private BooleanExpression productEq(List<String> products) {
-        BooleanExpression productExpression = null;
-        if (products != null && !products.isEmpty()) {
-            for (String product : products) {
-                BooleanExpression condition = brand.brandProduct.eq(product);
-                productExpression = (productExpression != null) ? productExpression.or(condition) : condition;
-            }
-        }
-        return productExpression;
+    private BooleanExpression priceEq(String price) {
+        return hasText(price) ? brand.brandPrice.eq(price) : null;
     }
 
-    private BooleanExpression ageEq(List<String> ages) {
-        BooleanExpression ageExpression = null;
-        if (ages != null && !ages.isEmpty()) {
-            for (String age : ages) {
-                BooleanExpression condition = brand.brandAge.eq(age);
-                ageExpression = (ageExpression != null) ? ageExpression.or(condition) : condition;
-            }
-        }
-        return ageExpression;
+    private BooleanExpression ageEq(String age) {
+        return hasText(age) ? brand.brandAge.eq(age) : null;
     }
 
-    private BooleanExpression priceEq(List<String> prices) {
-        BooleanExpression priceExpression = null;
-        if (prices != null && !prices.isEmpty()) {
-            for (String price : prices) {
-                BooleanExpression condition = brand.brandPrice.eq(price);
-                priceExpression = (priceExpression != null) ? priceExpression.or(condition) : condition;
-            }
-        }
-        return priceExpression;
+    private BooleanExpression productEq(String product) {
+        return hasText(product) ? brand.brandProduct.eq(product) : null;
     }
-
 }
+
+//    private BooleanExpression productEq(List<String> products) {
+//        BooleanExpression productExpression = null;
+//        if (products != null && !products.isEmpty()) {
+//            for (String product : products) {
+//                BooleanExpression condition = brand.brandProduct.eq(product);
+//                productExpression = (productExpression != null) ? productExpression.or(condition) : condition;
+//                // null이라면 첫번째 조건이므로 조건을 할당, null이 아니라면 이전에 생성된 조건에 or처리
+//            }
+//        }
+//        return productExpression;
+//    }
+//
+//    private BooleanExpression ageEq(List<String> ages) {
+//        BooleanExpression ageExpression = null;
+//        if (ages != null && !ages.isEmpty()) {
+//            for (String age : ages) {
+//                BooleanExpression condition = brand.brandAge.eq(age);
+//                ageExpression = (ageExpression != null) ? ageExpression.or(condition) : condition;
+//            }
+//        }
+//        return ageExpression;
+//    }
+//
+//    private BooleanExpression priceEq(List<String> prices) {
+//        BooleanExpression priceExpression = null;
+//        if (prices != null && !prices.isEmpty()) {
+//            for (String price : prices) {
+//                BooleanExpression condition = brand.brandPrice.eq(price);
+//                priceExpression = (priceExpression != null) ? priceExpression.or(condition) : condition;
+//            }
+//        }
+//        return priceExpression;
+//    }
+
+
