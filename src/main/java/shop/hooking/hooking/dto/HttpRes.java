28a1d699ec1def9dfc19d57df24f77281862a324
CYJhub <yejee1018@naver.com>
2023-07-10 23:41:00 +0900

    [feat] 오류일 때 반환 형식 통일

--- src/main/java/shop/hooking/hooking/dto/HttpRes.java
+++ src/main/java/shop/hooking/hooking/dto/HttpRes.java
@@ -6,22 +6,25 @@ import lombok.Data;
 
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
-@JsonPropertyOrder({"code", "message"})
+@JsonPropertyOrder({"code", "message", "data"})
 public class HttpRes<T> {
+
     private int code;
-    private T message;
+    private String message;
+    private T data;
 
 
     //오류일 경우
-    public HttpRes(int code, T message) {
+    public HttpRes(int code, String message) {
         this.code = code;
         this.message = message;
     }
 
 
     //성공일 경우
-    public HttpRes(T message) {
+    public HttpRes(T data) {
         this.code = 200;
-        this.message = message;
+        this.message = "요청에 성공하였습니다";
+        this.data = data;
     }
 }
