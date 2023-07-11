a09aeb4dda3e1d3c6abd6e379057817c2a2547ee
CYJhub <yejee1018@naver.com>
2023-07-12 00:57:35 +0900

    [feat] 크롤링 테스트 api구현

--- src/main/java/shop/hooking/hooking/dto/request/CrawlingReq.java
+++ src/main/java/shop/hooking/hooking/dto/request/CrawlingReq.java
@@ -1,17 +1,15 @@
 package shop.hooking.hooking.dto.request;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import shop.hooking.hooking.entity.Card;
+import java.util.List;
 
-import java.time.LocalDateTime;
-
-@Getter
-@NoArgsConstructor
 public class CrawlingReq {
-    private String text;
-    private LocalDateTime createdAt;
+    private List<CrawlingData> data;
 
+    public List<CrawlingData> getData() {
+        return data;
+    }
 
+    public void setData(List<CrawlingData> data) {
+        this.data = data;
+    }
 }
