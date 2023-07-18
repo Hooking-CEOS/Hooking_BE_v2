b8e46d6d544ab1d876089dc34e93eca75391b132
Jiwon <you@example.com>
2023-07-18 15:34:08 +0900

    [feat] 리다이렉트 주소 설정

--- src/main/java/shop/hooking/hooking/entity/Card.java
+++ src/main/java/shop/hooking/hooking/entity/Card.java
@@ -3,11 +3,15 @@ package shop.hooking.hooking.entity;
 import com.sun.istack.NotNull;
 import lombok.*;
 import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
 import org.springframework.data.annotation.CreatedDate;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
+@DynamicInsert
+@DynamicUpdate
 @Getter
 @Setter
 @NoArgsConstructor(access = AccessLevel.PUBLIC, force = true)
@@ -26,6 +30,8 @@ public class Card {
     @Column(name = "url")
     private String url;
 
+
+    @NotNull
     @Column(name = "scrap_cnt")
     @ColumnDefault("0")
     private Integer scrapCnt;
@@ -37,10 +43,15 @@ public class Card {
     @Column(name = "created_at")
     private LocalDateTime createdAt;
 
+    @PrePersist
+    public void prePersist() {
+        this.scrapCnt= this.scrapCnt == null ? 0 : this.scrapCnt;
+    }
+
     @Builder
     public Card(String text, Integer scrapCnt, Brand brand, LocalDateTime createdAt,String url) {
         this.text = text;
-        this.scrapCnt = scrapCnt;
+        this.scrapCnt = (scrapCnt!=null)?scrapCnt:0;
         this.brand = brand;
         this.createdAt = createdAt;
         this.url=url;
