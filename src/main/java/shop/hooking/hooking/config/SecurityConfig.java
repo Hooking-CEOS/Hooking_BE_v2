1d61a11c97103fc71bc909a5cfe8b77c778beae0
CYJhub <yejee1018@naver.com>
2024-02-04 14:56:42 +0900

    [feat] loginpage 명세

--- src/main/java/shop/hooking/hooking/config/SecurityConfig.java
+++ src/main/java/shop/hooking/hooking/config/SecurityConfig.java
@@ -93,7 +93,7 @@ public class SecurityConfig {
                 .logout()
                 .logoutSuccessUrl("/")
                 .and()
-                .oauth2Login()//인가코드 > 토큰받아옴 > 유저정보 요청(여기까지 자동으로 loadUser해줌) > 유저정보 받아옴
+                .oauth2Login().loginPage("/login/oauth2/code/kakao")//인가코드 > 토큰받아옴 > 유저정보 요청(여기까지 자동으로 loadUser해줌) > 유저정보 받아옴
                 .successHandler(authenticationSuccessHandler) //전체로그인 성공 시, handler를 설정
                 .userInfoEndpoint() //OAuth 2 로그인 성공 이후 사용자 정보를 가져올 때의 설정
                 .userService(oAuthUserService); //소설 로그인 성공 시 후속 조치를 진행할 UserService 인터페이스의 구현체를 등록
