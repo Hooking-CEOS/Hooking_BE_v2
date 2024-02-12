34f73daf032ebe7b2c1d5bb0659621a6813b7934
Jiwon <you@example.com>
2024-02-12 23:25:44 +0900

    [feat] 전체 브랜드 api 인덱스 5까지 가져오기 완성

--- src/main/java/shop/hooking/hooking/service/CopyService.java
+++ src/main/java/shop/hooking/hooking/service/CopyService.java
@@ -37,7 +37,7 @@ public class CopyService {
         Long[] brandIds = {1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L, 9L, 10L, 11L, 12L, 13L, 14L, 15L, 16L, 17L, 18L, 19L, 20L, 21L, 22L, 23L, 24L, 25L, 26L, 27L, 28L};
         List<CopyResDto> tempCopyRes = new ArrayList<>();
         for (Long brandId : brandIds) {
-            List<CopyResDto> copyRes = getTopSixCopy(brandId);
+            List<CopyResDto> copyRes = getTopEightCopy(brandId);
             tempCopyRes.addAll(copyRes);
         }
         Collections.shuffle(tempCopyRes);
@@ -82,8 +82,8 @@ public class CopyService {
     }
 
     @Transactional
-    public List<CopyResDto> getTopSixCopy(Long brandId) {
-        List<Card> cards = cardRepository.findTop6ByBrandIdOrderByCreatedAtDesc(brandId);
+    public List<CopyResDto> getTopEightCopy(Long brandId) {
+        List<Card> cards = cardRepository.findTop8ByBrandIdOrderByCreatedAtDesc(brandId);
         List<CopyResDto> copyResList = new ArrayList<>();
 
         for (Card card : cards) {
